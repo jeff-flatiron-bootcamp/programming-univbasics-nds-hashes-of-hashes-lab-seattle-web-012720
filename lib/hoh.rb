1be9b1baf3d4a01a4cc0c2d7105d308669d4d363
Jeff Simon <jeffsbillinfo@gmail.com>
2020-01-09 03:38:50 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -52,9 +52,25 @@ def naming_system
         sub_category: 
         {
             label: "Class"
-        
+            sub_category:
+            {
+              label: "Order"
+              sub_category:
+              {
+                label: "Family"
+                sub_category:
+                {
+                  label: "Genus"
+                  sub_category: 
+                  {
+                    label: "Species"
+                  }
+                }
+              }
+            }
+          }
         }
       }
-    }
+    
     
 end

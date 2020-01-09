af407fb13d90b3582076ec9c94176d2a2f45496c
Jeff Simon <jeffsbillinfo@gmail.com>
2020-01-09 03:39:25 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -51,16 +51,16 @@ def naming_system
         label: "Phylum", 
         sub_category: 
         {
-            label: "Class"
+            label: "Class",
             sub_category:
             {
-              label: "Order"
+              label: "Order",
               sub_category:
               {
-                label: "Family"
+                label: "Family",
                 sub_category:
                 {
-                  label: "Genus"
+                  label: "Genus",
                   sub_category: 
                   {
                     label: "Species"
